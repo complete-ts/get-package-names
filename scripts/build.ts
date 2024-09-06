b2fb585452b9aff4d6aa1f88986ed75843022176
Zamiell <5511220+Zamiell@users.noreply.github.com>
2024-09-06 10:11:07 -0400

    fix: ESM output

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,5 +1,5 @@
 import { $s, buildScript } from "isaacscript-common-node";
 
 await buildScript(() => {
-  $s`esbuild --bundle --outfile=./dist/main.js --platform=node ./src/main.ts`;
+  $s`esbuild --bundle --outfile=./dist/main.js --platform=node --format=esm ./src/main.ts`;
 });
