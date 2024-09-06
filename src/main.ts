188cb4c5328a6b8e5ff4bd5ddb7cfef1e51adb6b
Zamiell <5511220+Zamiell@users.noreply.github.com>
2024-09-06 09:16:17 -0400

    feat: catch errors

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import * as core from "@actions/core";
 import { getMonorepoPackageNames } from "./getMonorepoPackageNames.js";
 
-const cwd = process.cwd();
-console.log("DEBUG cwd:", cwd);
-const scriptName = core.getInput("script-name"); // Must match the value in "action.yml"
-console.log("DEBUG scriptFilter:", scriptName);
-const packageNames = getMonorepoPackageNames(cwd, scriptName);
-core.setOutput("package-names", packageNames); // Must match the value in "action.yml"
+try {
+  main();
+} catch (error) {
+  const message = error instanceof Error ? error.message : "unknown error";
+  core.setFailed(message);
+}
+
+function main() {
+  const cwd = process.cwd();
+  console.log("DEBUG cwd:", cwd);
+  const scriptName = core.getInput("script-name"); // Must match the value in "action.yml"
+  console.log("DEBUG scriptFilter:", scriptName);
+  const packageNames = getMonorepoPackageNames(cwd, scriptName);
+  core.setOutput("package-names", packageNames); // Must match the value in "action.yml"
+}
