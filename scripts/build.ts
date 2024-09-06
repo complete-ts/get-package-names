3c7cefa70b6064c1834d766318adc1544c4cd351
Zamiell <5511220+Zamiell@users.noreply.github.com>
2024-09-06 10:50:04 -0400

    fix: CJS

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,5 +1,12 @@
-import { $s, buildScript } from "isaacscript-common-node";
+import { build } from "esbuild"; // eslint-disable-line import/no-extraneous-dependencies
 
-await buildScript(() => {
-  $s`esbuild --bundle --outfile=./dist/main.js --platform=node --format=esm ./src/main.ts`;
+// - We cannot use top-level await because the project uses CommonJS.
+// - We have to use CommonJS because esbuild generates a bundle with the following run-time error
+//   otherwise: Error: Dynamic require of "os" is not supported
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
+build({
+  entryPoints: ["./src/main.ts"],
+  bundle: true,
+  outfile: "./dist/main.js",
+  platform: "node",
 });
