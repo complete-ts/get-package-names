43803710e344c33282e403fd4a709096bde97a62
Zamiell <5511220+Zamiell@users.noreply.github.com>
2024-09-06 08:18:02 -0400

    feat: wip get directory list

---
+++ src/getMonorepoPackageNames.ts
@@ -0,0 +1,50 @@
+import fs from "node:fs";
+
+export function getMonorepoPackageNames(
+  cwd: string,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  scriptName?: string,
+): readonly string[] {
+  const fileList = fs.readdirSync(cwd);
+  console.log("DEBUG fileList:", fileList);
+
+  return [];
+
+  /*
+  const fromDirToUse = fromDir ?? dirOfCaller();
+  const monorepoRoot = findPackageRoot(fromDirToUse);
+  const packagesPath = path.join(monorepoRoot, "packages");
+  if (!isDirectory(packagesPath)) {
+    throw new Error(
+      `The monorepo packages directory does not exist at: ${packagesPath}`,
+    );
+  }
+
+  const packageNames: string[] = [];
+
+  const fileNames = getFileNamesInDirectory(packagesPath);
+  for (const fileName of fileNames) {
+    const filePath = path.join(packagesPath, fileName);
+    if (isDirectory(filePath)) {
+      packageNames.push(fileName);
+    }
+  }
+
+  if (scriptName === undefined || scriptName === "") {
+    return packageNames;
+  }
+
+  return packageNames.filter((packageName) => {
+    const packageJSONPath = path.join(
+      packagesPath,
+      packageName,
+      "package.json",
+    );
+    if (!isFile(packageJSONPath)) {
+      return false;
+    }
+
+    return packageJSONHasScript(packageJSONPath, scriptName);
+  });
+  */
+}
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
-main();
+import core from "@actions/core";
+import { getMonorepoPackageNames } from "./getMonorepoPackageNames.js";
 
-function main() {
-  console.log("Hello world.");
-}
+const cwd = process.cwd();
+console.log("DEBUG cwd:", cwd);
+const scriptName = core.getInput("scriptName");
+console.log("DEBUG scriptFilter:", scriptName);
+const packageNames = getMonorepoPackageNames(cwd, scriptName);
+core.setOutput("packageNames", packageNames);
