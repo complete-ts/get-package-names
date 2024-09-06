f864fe5d345f81fa6e188942ccec71da00d9f72a
Zamiell <5511220+Zamiell@users.noreply.github.com>
2024-09-06 08:35:20 -0400

    refactor: rename vars

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { getMonorepoPackageNames } from "./getMonorepoPackageNames.js";
 
 const cwd = process.cwd();
 console.log("DEBUG cwd:", cwd);
-const scriptName = core.getInput("scriptName");
+const scriptName = core.getInput("script-name"); // Must match the value in "action.yml"
 console.log("DEBUG scriptFilter:", scriptName);
 const packageNames = getMonorepoPackageNames(cwd, scriptName);
-core.setOutput("packageNames", packageNames);
+core.setOutput("package-names", packageNames); // Must match the value in "action.yml"
